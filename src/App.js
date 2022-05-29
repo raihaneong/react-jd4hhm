fdb5a81bbb794dcb64cd946cb8f5a1b8925fb4d1
raihaneong <raihneong@gmail.com>
2022-05-30 04:14:21 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import { Box, NumberBox } from './components';
 
 export default function App() {
   const [box, setBox] = useState(boxes);
+  const [boxNumber, setBoxNumber] = useState(boxes.id)
   const boxElement = box.map((item) => (
     <Box key={item.id} on={item.on} number={item.id} />
   ));
+  const boxNumberElement = box.map((item) => (
+    <NumberBox id={boxNumber} />
+  ))
+
+  console.log(box)
   return (
     <div>
       <h1>flipping box</h1>
       {boxElement}
-      <NumberBox number={boxes.id} />
+
     </div>
   );
 }
